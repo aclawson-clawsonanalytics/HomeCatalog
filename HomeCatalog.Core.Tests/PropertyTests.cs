0e1fb56a2f0c70ff48223332798ad05764a54309
Andrew Clawson <andrew.clawson@ucdenver.edu>
2013-07-18 00:59:59 -0600

    Add property fields for PropertyMetaData and performed unit tests.

--- HomeCatalog.Core.Tests/PropertyTests.cs
+++ HomeCatalog.Core.Tests/PropertyTests.cs
@@ -6,18 +6,23 @@ namespace HomeCatalog.Core.Tests
 	[TestFixture()]
 	public class PropertyTests
 	{
+		private Property sut = new Property();
+
+		[SetUp()]
+		public void SetUp ()
+		{
+			Property sut = new Property ();
+		}
+
 		[Test()]
 		public void PropertyHasName ()
 		{
-			Property sut = new Property ();
-			sut.PropertyName = "Home";
-			Assert.That (sut.PropertyName != null);
+			Assert.That (sut.PropertyName == null);
 		}
 
 		[Test()]
 		public void PropertyNameIsCorrect()
 		{
-			Property sut = new Property ();
 			sut.PropertyName = "Name";
 			Assert.That (sut.PropertyName == "Name");
 		}
@@ -25,8 +30,46 @@ namespace HomeCatalog.Core.Tests
 		[Test]
 		public void PropertyHasID()
 		{
-			Property sut = new Property ();
 			Assert.That (sut.PropertyID != null);
+		}
+
+		[Test()]
+		public void PropertyHasAddress()
+		{
+			Assert.That (sut.Address != null);
+		}
+
+		[Test ()]
+		public void PropertyAddressIsCorrect ()
+		{
+			sut.Address = "19859 E. Garden Pl";
+			Assert.That (sut.Address == "19859 E. Garden Pl");
+		}
+
+		[Test()]
+		public void PropertyHasCity ()
+		{
+			Assert.That (sut.City != null);
+		}
+
+		[Test()]
+		public void PropertyCityIsCorrect ()
+		{
+			sut.City = "Centennial";
+			Assert.That (sut.City == "Centennial");
+		}
+
+		[Test()]
+		public void PropertyHasState ()
+		{
+			Assert.That (sut.State == null);
+		}
+
+		[Test()]
+		public void PropertyStateIsCorrect()
+		{
+			sut.State = "Colorado";
+			Assert.That (sut.State== "Colorado");
 		}
 	}
 }
--- HomeCatalog.Core/Property.cs
+++ HomeCatalog.Core/Property.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace HomeCatalog.Core
 {
@@ -9,8 +11,14 @@ namespace HomeCatalog.Core
 			PropertyID = Guid.NewGuid ().ToString ();
 		}
 
+		// Define Metadata elements
 		public string PropertyID { get; private set; }
-		public string PropertyName { get; set;}
+		public string PropertyName { get; set; }
+		public string Address { get; set; }
+		public string City {get;set;}
+		public string State { get; set; }
+		public string ZipCode {get;set;}
+
 	}
 }
 
