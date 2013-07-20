0295c11ef75c190d05102fdf1ac02906f17c3176
Andrew Clawson <andrew.clawson@ucdenver.edu>
2013-07-20 06:55:24 -0600

    Wrote SetCheckBoxByRoom and CheckBoxClicker methods in EditRoomsActivity.  Finished wiring checkboxes to the view and defining click events for each box.

--- HomeCatalog.Android/HomeCatalog.Android/Activities/EditRoomsActivity.cs
+++ HomeCatalog.Android/HomeCatalog.Android/Activities/EditRoomsActivity.cs
@@ -53,46 +53,112 @@ namespace HomeCatalog.Android
 			CheckBox OfficeCheckBox = FindViewById<CheckBox> (Resource.Id.OfficeCheckBox);
 
 			// Set CheckBox checks for existing rooms in RoomList
-			KitchenCheckBox.Checked = SetCheckBoxByRoomLabel ("Kitchen");
-			LivingCheckBox.Checked = SetCheckBoxByRoomLabel ("Living Room");
-			StorageCheckBox.Checked = SetCheckBoxByRoomLabel ("Storage");
-			BasementCheckBox.Checked = SetCheckBoxByRoomLabel ("Basement");
-			OfficeCheckBox.Checked = SetCheckBoxByRoomLabel ("Office");
+			KitchenCheckBox.Checked = SetCheckBoxByRoom (Kitchen);
+			LivingCheckBox.Checked = SetCheckBoxByRoom (Living);
+			StorageCheckBox.Checked = SetCheckBoxByRoom (Storage);
+			BasementCheckBox.Checked = SetCheckBoxByRoom (Basement);
+			OfficeCheckBox.Checked = SetCheckBoxByRoom (Office);
 
 			// Set Click events for CheckBoxes
 			KitchenCheckBox.Click += (o,e) => 
 			{
-				if (KitchenCheckBox.Checked = false)
-				{
-					if (Property.RoomList.Contains (Kitchen)	// if not checked, check and add kit
-					{
-						Room Kitchen = new Room ();
-						Kitchen.RoomID = 1;
-						Kitchen.Label = "Kitchen";
-					}
-					else
-					{
+				CheckBoxClicker (KitchenCheckBox, "Kitchen");
+			};
 
-					}
-				}
+			LivingCheckBox.Click += (o,e) =>
+			{
+				CheckBoxClicker (LivingCheckBox,"Living Room");
+			};
 
-			}
+			StorageCheckBox.Click += (o,e) =>
+			{
+				CheckBoxClicker (StorageCheckBox,"Storage");
+			};
+
+			BasementCheckBox.Click += (o,e) =>
+			{
+				CheckBoxClicker (BasementCheckBox, "Living Room");
+			};
+
+			OfficeCheckBox.Click += (o,e) =>
+			{
+				CheckBoxClicker (OfficeCheckBox,"Office");
+			};
+
+
 		
 		}
 
-		private CheckBox CheckBoxClicker (CheckBox cb,Room room)
+		private void CheckBoxClicker (CheckBox cb,string label)
 		{
-			if (cb.Checked = false)
+			if (cb.Checked == false)
 			{
-					
+				foreach (Room rm in Property.RoomList)
+				{
+					if (rm.Label == label)
+					{
+						Property.RoomList.Remove (rm);
+					}
+				}
 			}
+			else
+			{
+				int count = 0;
+				foreach (Room rm in Property.RoomList)
+				{
+					if (rm.Label == label)
+					{
+						count = count + 1;
+					}
+				}
 
+				if (count == 0) 
+				{
+					Room room = new Room ();
+					switch (label)
+					{
+					case "Kitchen":
+							
+						room.RoomID = 1;
+						room.Label = "Kitchen";
+						Property.RoomList.Add (room);
+					case "LivingRoom":
+						room.RoomID = 2;
+						Room.Label = "Living Room";
+						Property.RoomList.Add (room);
+					case "Storage":
+						room.RoomID = 3;
+						room.Label = "Storage";
+						Property.RoomList.Add (room);
+					case "Basement":
+						room.RoomID = 4;
+						room.Label = "Basement";
+						Property.RoomList.Add (room);
+					case "Office":
+						room.RoomID = 5;
+						room.Label = "Office";
+						Property.RoomList.Add (room);
+					}
+				}
+			}
 		}
 
-		private Room AddRoomByCheckBox (CheckBox checkbox)
+
+		private bool SetCheckBoxByRoom (Room room)
+		{
+			if (Property.RoomList.Contains(Room)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		private void RemoveRoomByCheckBox (CheckBox checkbox)
 		{
 
-		}
 
 
 
