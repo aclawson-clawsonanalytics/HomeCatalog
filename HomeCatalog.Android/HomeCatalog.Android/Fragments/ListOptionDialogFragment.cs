153e84be9769bbd78b555e005df024ff4976dc23
Andrew Clawson <andrew.clawson@ucdenver.edu>
2013-08-17 20:45:04 -0600

    Added ListOptionDialogFragment to project for removing items from lists.

---
+++ HomeCatalog.Android/HomeCatalog.Android/Fragments/ListOptionDialogFragment.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+
+namespace HomeCatalog.Android
+{
+	public class ListOptionDialogFragment : DialogFragment
+	{
+		private string[] optionList;
+
+		public override Dialog OnCreateDialog(Bundle savedInstanceState)
+		{
+			optionList = new string[] {"Edit","Delete"};
+			var builder = new AlertDialog.Builder (Activity);
+			builder.SetTitle ("How to add the photo: ");
+			builder.SetItems (optionList, delegate(object sender, DialogClickEventArgs e) {
+				});
+
+			return builder.Create ();
+		}
+	}
+}
+
