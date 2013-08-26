04890f0dab01e43c060dd5e382537eb742963c2d
Andrew Clawson <andrew.clawson@ucdenver.edu>
2013-08-25 20:06:01 -0600

    DeleteDialog implemented for RoomList

--- HomeCatalog.Android/HomeCatalog.Android/Fragments/DeleteDialogFragment.cs
+++ HomeCatalog.Android/HomeCatalog.Android/Fragments/DeleteDialogFragment.cs
@@ -8,6 +8,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using HomeCatalog.Core;
 
 
 namespace HomeCatalog.Android
@@ -15,19 +16,21 @@ namespace HomeCatalog.Android
 	public class DeleteDialogFragment : DialogFragment
 	{
 		private string[] optionList;
+		private Property Property { get; set; }
+		public delegate void OnItemSelectedDelegate (DialogClickEventArgs e);
+		public OnItemSelectedDelegate OnItemSelected {get; set;}
+
 		public override Dialog OnCreateDialog(Bundle savedInstanceState)
 		{
-			optionList = new string[] {"Rename","Delete"};
+			optionList = new string[] {"Delete"};
+			Property = PropertyStore.CurrentStore.Property;
+
 			var builder = new AlertDialog.Builder (Activity);
-			builder.SetTitle ("DELETE ITEM");
-			builder.SetMessage ("Are you sure?");
-
-			// Set buttons
-			builder.SetPositiveButton ("Ok", (sender,EventArgs) =>
+			builder.SetTitle ("Delete");
+			builder.SetItems (optionList,delegate(object sender, DialogClickEventArgs e)
 			{
-
+				OnItemSelected(e);
 			});
-			                          
 
 			return builder.Create ();
 		}
