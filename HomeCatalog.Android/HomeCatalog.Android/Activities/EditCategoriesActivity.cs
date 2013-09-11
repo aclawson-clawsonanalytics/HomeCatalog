b0b721665668aa60f6137aaeab8c829923e45c63
Andrew Clawson <andrew.clawson@ucdenver.edu>
2013-09-10 19:51:10 -0600

    Work on EditCategoriesActivity and CategoryList.cs in Core

--- HomeCatalog.Android/HomeCatalog.Android/Activities/EditCategoriesActivity.cs
+++ HomeCatalog.Android/HomeCatalog.Android/Activities/EditCategoriesActivity.cs
@@ -101,7 +101,7 @@ namespace HomeCatalog.Android
 
 		}
 
-		private bool CheckForCategoryByLabel(Property prop, string label)
+		private bool CheckForCategoryByLabel(string label)
 		{
 			int count = 0;
 			foreach (Category cat in Property.CategoryList.AllItems ())
@@ -121,184 +121,218 @@ namespace HomeCatalog.Android
 			}
 		}
 
-		private void SaveCategories()
+		private void SaveCategoryFromCheckBox(string label,CheckBox check)
 		{
-			//Save Appliance
-			if (ApplianceCheckBox.Checked == true)
+			if (check.Checked == true && CheckForCategoryByLabel(label) == false)
 			{
-				//If not found, add to Property.RoomList
-				//If found, don't do anything
-				if (CheckForCategoryByLabel (Property,"Kitchen") == false)
-				{
-					Category Appliance = new Category ();
-					Appliance.Label = "Appliance";
-					Property.CategoryList.Add (Appliance);
-				}
+				Category newCategory = new Room ();
+				newCategory.Label = label;
+				Property.CategoryList.Add (newCategory);
 			}
-			//If box.checked == false, check for room and delete if existing
-			else
+			else if (check.Checked == false && CheckForCategoryByLabel(label) == true)
 			{
-				if (CheckForCategoryByLabel (Property, "Appliance") == true)
-				{
-					RemoveByCategoryLabel("Appliance");
-				}
-			}
-
-			//Save BathAppliance
-			if (BathApplianceCheckBox.Checked == true)
-			{
-				//If not found, add to Property.RoomList
-				//If found, don't do anything
-				if (CheckForCategoryByLabel (Property,"Bathroom Appliance") == false)
-				{
-					Category BathroomAppliance = new Category ();
-					BathroomAppliance.Label = "Bathroom Appliance";
-					Property.CategoryList.Add (BathroomAppliance);
-				}
-				else
-				{
-					RemoveByCategoryLabel("Bathroom Appliance");
-				}
-			}
-
-			// Save Dishes
-			if (DishCheckBox.Checked == true)
-			{
-				//If not found, add to Property.RoomList
-				//If found, don't do anything
-				if (CheckForCategoryByLabel (Property,"Dishes") == false)
-				{
-					Category Dishes = new Category ();
-					Dishes.Label = "Dishes";
-					Property.CategoryList.Add (Dishes);
-				}
-				else
-				{
-					RemoveByCategoryLabel("Dishes");
-				}
-			}
-
-			// Electronics
-			if (ElectronicsCheckBox.Checked == true)
-			{
-				//If not found, add to Property.RoomList
-				//If found, don't do anything
-				if (CheckForCategoryByLabel (Property,"Electronics") == false)
-				{
-					Category Electronics = new Category ();
-					Electronics.Label = "Electronics";
-					Property.CategoryList.Add (Electronics);
-				}
-				else
-				{
-					RemoveByCategoryLabel("Electronics");
-				}
-			}
-
-			//Furniture
-			if (FurnitureCheckBox.Checked == true)
-			{
-				//If not found, add to Property.RoomList
-				//If found, don't do anything
-				if (CheckForCategoryByLabel (Property,"Furniture") == false)
-				{
-					Category Furniture = new Category ();
-					Furniture.Label = "Furniture";
-					Property.CategoryList.Add (Furniture);
-				}
-				else
-				{
-					RemoveByCategoryLabel ("Furniture");
-				}
-			}
-
-			//Kitchen Appliance
-			if (KitchenApplianceCheckBox.Checked == true)
-			{
-				//If not found, add to Property.RoomList
-				//If found, don't do anything
-				if (CheckForCategoryByLabel (Property,"Kitchen Appliance") == false)
-				{
-					Category KitchenAppliance = new Category ();
-					KitchenAppliance.Label = "Kitchen Appliance";
-					Property.CategoryList.Add (KitchenAppliance);
-				}
-				else
-				{
-					RemoveByCategoryLabel("Kitchen Appliance");
-				}
-			}
-
-			if (StorageCheckBox.Checked == true)
-			{
-				//If not found, add to Property.RoomList
-				//If found, don't do anything
-				if (CheckForCategoryByLabel (Property,"Storage") == false)
-				{
-					Category Storage = new Category ();
-					Storage.Label = "Storage";
-					Property.CategoryList.Add (Storage);
-				}
-				else
-				{
-					RemoveByCategoryLabel ("Storage");
-				}
-			}
-
-			if (ToolsCheckBox.Checked == true)
-			{
-				//If not found, add to Property.RoomList
-				//If found, don't do anything
-				if (CheckForCategoryByLabel (Property,"Tools") == false)
-				{
-					Category Tools = new Category ();
-					Tools.Label = "Tools";
-					Property.CategoryList.Add (Tools);
-				}
-				else
-				{
-					RemoveByCategoryLabel ("Tools");
-				}
-			}
-
-			SaveCustomCategory ();
-
-		}
-
-
-		private bool SetCheckBoxByCategory (string label)
-		{
-			if (CheckForCategoryByLabel (Property,label) == true)
-			{
-				return true;
-			}
-			else
-			{
-				return false;
-			}
-
-		}
-
-		private void SaveCustomCategory ()
-		{
-			if (CustomCategoryField.Text != "")
-			{
-				Category NewCategory = new Category ();
-				NewCategory.Label = CustomCategoryField.Text;
+				Category cat = Property.CategoryList.CategoryWithName (label);
+				Property.CategoryList.Remove (cat);
 			}
 		}
-
-
-		private void RemoveByCategoryLabel (string label)
-		{
-			foreach (Category cat in Property.CategoryList.AllItems ())
-			{
-				if (cat.Label == label)
-				{
-					Property.CategoryList.Remove (cat);
-				}
-			}
-		}
+//		private bool CheckForCategoryByLabel(Property prop, string label)
+//		{
+//			int count = 0;
+//			foreach (Category cat in Property.CategoryList.AllItems ())
+//			{
+//				if (cat.Label == label)
+//				{
+//					count = count + 1;
+//				}
+//			}
+//			if (count == 0)
+//			{
+//				return false;
+//			}
+//			else
+//			{
+//				return true;
+//			}
+//		}
+//
+//		private void SaveCategories()
+//		{
+//			//Save Appliance
+//			if (ApplianceCheckBox.Checked == true)
+//			{
+//				//If not found, add to Property.RoomList
+//				//If found, don't do anything
+//				if (CheckForCategoryByLabel (Property,"Kitchen") == false)
+//				{
+//					Category Appliance = new Category ();
+//					Appliance.Label = "Appliance";
+//					Property.CategoryList.Add (Appliance);
+//				}
+//			}
+//			//If box.checked == false, check for room and delete if existing
+//			else
+//			{
+//				if (CheckForCategoryByLabel (Property, "Appliance") == true)
+//				{
+//					RemoveByCategoryLabel("Appliance");
+//				}
+//			}
+//
+//			//Save BathAppliance
+//			if (BathApplianceCheckBox.Checked == true)
+//			{
+//				//If not found, add to Property.RoomList
+//				//If found, don't do anything
+//				if (CheckForCategoryByLabel (Property,"Bathroom Appliance") == false)
+//				{
+//					Category BathroomAppliance = new Category ();
+//					BathroomAppliance.Label = "Bathroom Appliance";
+//					Property.CategoryList.Add (BathroomAppliance);
+//				}
+//				else
+//				{
+//					RemoveByCategoryLabel("Bathroom Appliance");
+//				}
+//			}
+//
+//			// Save Dishes
+//			if (DishCheckBox.Checked == true)
+//			{
+//				//If not found, add to Property.RoomList
+//				//If found, don't do anything
+//				if (CheckForCategoryByLabel (Property,"Dishes") == false)
+//				{
+//					Category Dishes = new Category ();
+//					Dishes.Label = "Dishes";
+//					Property.CategoryList.Add (Dishes);
+//				}
+//				else
+//				{
+//					RemoveByCategoryLabel("Dishes");
+//				}
+//			}
+//
+//			// Electronics
+//			if (ElectronicsCheckBox.Checked == true)
+//			{
+//				//If not found, add to Property.RoomList
+//				//If found, don't do anything
+//				if (CheckForCategoryByLabel (Property,"Electronics") == false)
+//				{
+//					Category Electronics = new Category ();
+//					Electronics.Label = "Electronics";
+//					Property.CategoryList.Add (Electronics);
+//				}
+//				else
+//				{
+//					RemoveByCategoryLabel("Electronics");
+//				}
+//			}
+//
+//			//Furniture
+//			if (FurnitureCheckBox.Checked == true)
+//			{
+//				//If not found, add to Property.RoomList
+//				//If found, don't do anything
+//				if (CheckForCategoryByLabel (Property,"Furniture") == false)
+//				{
+//					Category Furniture = new Category ();
+//					Furniture.Label = "Furniture";
+//					Property.CategoryList.Add (Furniture);
+//				}
+//				else
+//				{
+//					RemoveByCategoryLabel ("Furniture");
+//				}
+//			}
+//
+//			//Kitchen Appliance
+//			if (KitchenApplianceCheckBox.Checked == true)
+//			{
+//				//If not found, add to Property.RoomList
+//				//If found, don't do anything
+//				if (CheckForCategoryByLabel (Property,"Kitchen Appliance") == false)
+//				{
+//					Category KitchenAppliance = new Category ();
+//					KitchenAppliance.Label = "Kitchen Appliance";
+//					Property.CategoryList.Add (KitchenAppliance);
+//				}
+//				else
+//				{
+//					RemoveByCategoryLabel("Kitchen Appliance");
+//				}
+//			}
+//
+//			if (StorageCheckBox.Checked == true)
+//			{
+//				//If not found, add to Property.RoomList
+//				//If found, don't do anything
+//				if (CheckForCategoryByLabel (Property,"Storage") == false)
+//				{
+//					Category Storage = new Category ();
+//					Storage.Label = "Storage";
+//					Property.CategoryList.Add (Storage);
+//				}
+//				else
+//				{
+//					RemoveByCategoryLabel ("Storage");
+//				}
+//			}
+//
+//			if (ToolsCheckBox.Checked == true)
+//			{
+//				//If not found, add to Property.RoomList
+//				//If found, don't do anything
+//				if (CheckForCategoryByLabel (Property,"Tools") == false)
+//				{
+//					Category Tools = new Category ();
+//					Tools.Label = "Tools";
+//					Property.CategoryList.Add (Tools);
+//				}
+//				else
+//				{
+//					RemoveByCategoryLabel ("Tools");
+//				}
+//			}
+//
+//			SaveCustomCategory ();
+//
+//		}
+//
+//
+//		private bool SetCheckBoxByCategory (string label)
+//		{
+//			if (CheckForCategoryByLabel (Property,label) == true)
+//			{
+//				return true;
+//			}
+//			else
+//			{
+//				return false;
+//			}
+//
+//		}
+//
+//		private void SaveCustomCategory ()
+//		{
+//			if (CustomCategoryField.Text != "")
+//			{
+//				Category NewCategory = new Category ();
+//				NewCategory.Label = CustomCategoryField.Text;
+//			}
+//		}
+//
+//
+//		private void RemoveByCategoryLabel (string label)
+//		{
+//			foreach (Category cat in Property.CategoryList.AllItems ())
+//			{
+//				if (cat.Label == label)
+//				{
+//					Property.CategoryList.Remove (cat);
+//				}
+//			}
+//		}
 		
 		
 	}
