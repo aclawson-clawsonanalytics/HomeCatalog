4a293a903386a9a552d247ae9e00ca53c52a58a4
Andrew Clawson <andrew.clawson@ucdenver.edu>
2013-10-17 20:50:21 -0600

    Added TakePhoto method to PhotoBrowserActivity. Added to PhotoDialogFragment methods to launch the camera.

--- HomeCatalog.Android/HomeCatalog.Android/Activities/CameraActivity.cs
+++ HomeCatalog.Android/HomeCatalog.Android/Activities/CameraActivity.cs
@@ -8,17 +8,38 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Provider;
+using Java.IO;
 
 namespace HomeCatalog.Android
 {
 	[Activity (Label = "Camera")]			
 	public class CameraActivity : Activity
 	{
+		Java.IO.File _file;
+		Java.IO.File _dir;
+		ImageView _imageView;
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
+			//SetContentView (Resource.Layout.CameraView);
 
-			// Create your application here
+
+		}
+
+//		private bool IsThereAnAppToTakePictures()
+//		{
+//			//Intent intent = new Intent (MediaStore.ActionImageCapture);
+//			//IList<ResolveInfo> availableActivities = PackageManager.QueryIntentActivities (intent, PackageInfoFlags.MatchDefaultOnly);
+//			//return availableActivities != null && availableActivities.Count > 0;
+//		}
+
+		private void TakeAPicture(object sender,EventArgs eventArgs)
+		{
+			Intent intent = new Intent (MediaStore.ActionImageCapture);
+			_file = new File (_dir, String.Format ("myPhoto_{0}.jpg", Guid.NewGuid ()));
+			//intent.PutExtra (MediaStore.ExtraOutput, Uri.FromFile (_file));
+			StartActivityForResult (intent, 0);
 		}
 	}
 }
--- HomeCatalog.Android/HomeCatalog.Android/Fragments/PhotoDialogFragment.cs
+++ HomeCatalog.Android/HomeCatalog.Android/Fragments/PhotoDialogFragment.cs
@@ -7,7 +7,9 @@ using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
+using Android.Provider;
 using Android.Widget;
+using Android.Content.PM;
 
 
 namespace HomeCatalog.Android
@@ -18,20 +20,56 @@ namespace HomeCatalog.Android
 
 		public override Dialog OnCreateDialog(Bundle savedInstanceState)
 		{
-			optionList = new string[] {"From Camera","From Gallery"};
+			if (IsThereAnAppToTakePictures())
+			{
+				optionList = new string[] {"From Camera","From Gallery"};
+			}
+			else
+			{
+				optionList = new string[] { "From Gallery" };
+			}
+
 			var builder = new AlertDialog.Builder (Activity);
 			builder.SetTitle ("How to add the photo: ");
 			builder.SetItems (optionList, delegate(object sender, DialogClickEventArgs e)
 			{
-//				switch (e.Which)
-//				{
-//					case 0:
-//						
-//
-//				}
+				if(IsThereAnAppToTakePictures())
+				{
+					switch (e.Which)
+					{
+						case 0:
+						TakePhoto ();
+						break;
+							
+
+					}
+				}
+
+				else
+				{
+
+				}
 			});
 
 			return builder.Create ();
+		}
+
+		private bool IsThereAnAppToTakePictures()
+		{
+			Intent intent = new Intent (MediaStore.ActionImageCapture);
+			IList<ResolveInfo> availableActivities = PackageManager.QueryIntentActivities (intent, PackageInfoFlags.MatchDefaultOnly);
+			return availableActivities != null && availableActivities.Count > 0;
+		}
+
+		private void TakePhoto()
+		{
+			Intent intent = new Intent(MediaStore.ActionImageCapture);
+
+			//_file = new File(_dir, String.Format("myPhoto_{0}.jpg", Guid.NewGuid()));
+			//
+			//intent.PutExtra(MediaStore.ExtraOutput, Uri.FromFile(_file));
+
+			StartActivityForResult(intent, 0);
 		}
 		}
 }
