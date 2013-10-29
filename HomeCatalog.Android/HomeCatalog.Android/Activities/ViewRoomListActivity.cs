9a113e194c65226ba9fd10b9a8598778cb8a59c0
Andrew Clawson <andrew.clawson@ucdenver.edu>
2013-10-29 00:53:04 -0600

    Re-worked RoomsView layout. Implemented changes in ViewRoomListActivity. Created RoomListDialogFragment to update user interface. Next steps:  - Create RoomView layout  - Wire list item click event and custom item click event to the RoomView layout

--- HomeCatalog.Android/HomeCatalog.Android/Activities/ViewRoomListActivity.cs
+++ HomeCatalog.Android/HomeCatalog.Android/Activities/ViewRoomListActivity.cs
@@ -28,37 +28,117 @@ namespace HomeCatalog.Android
 			ListAdapter = new RoomListAdapter (this,Property);
 			listView.Adapter = ListAdapter;
 
-			Button EditRoomsButton = FindViewById<Button> (Resource.Id.EditRoomsButton2);
-			EditRoomsButton.Click += (sender, e) => 
+			Button AddBathroomButton = FindViewById<Button> (Resource.Id.addBathroomButton);
+			AddBathroomButton.Click += (sender, e) => 
 			{
-				Intent PassPropertyID = new Intent (this,typeof(EditRoomsActivity));
-				PassPropertyID.PutExtra (Property.PropertyIDKey,Property.PropertyID);
-				StartActivity (PassPropertyID);
+//				Intent PassPropertyID = new Intent (this,typeof(EditRoomsActivity));
+//				PassPropertyID.PutExtra (Property.PropertyIDKey,Property.PropertyID);
+//				StartActivity (PassPropertyID);
 			};
 
-			Button BackButton = FindViewById<Button> (Resource.Id.BackButton);
-			BackButton.Click += (sender,e) =>
+			Button AddBedroomButton = FindViewById<Button> (Resource.Id.addBedroomButton);
+			AddBedroomButton.Click += (sender,e) =>
 			{
-				SetResult (Result.Canceled);
-				Finish ();
+//				SetResult (Result.Canceled);
+//				Finish ();
 			};
 
+			Button AddRoomButton = FindViewById<Button> (Resource.Id.addRoomButton);
+			AddRoomButton.Click += (sender,e) =>
+			{
+				//Insert Code for RoomListDialog
+			};
+
+//			listView.ItemClick += (Object sender, AdapterView.ItemClickEventArgs e) =>
+//			{
+//				var transaction = FragmentManager.BeginTransaction ();
+//				DeleteDialogFragment deleteDialog = new DeleteDialogFragment ();
+//				deleteDialog.Show (transaction, "deleteDialog");
+//
+//				deleteDialog.OnItemSelected += (DialogClickEventArgs a) =>
+//				{
+//					Property.RoomList.Remove (ListAdapter [e.Position]);
+//					ListAdapter.NotifyDataSetChanged ();
+//				};
+//			};
 			listView.ItemClick += (Object sender, AdapterView.ItemClickEventArgs e) =>
 			{
-				var transaction = FragmentManager.BeginTransaction ();
-				DeleteDialogFragment deleteDialog = new DeleteDialogFragment ();
-				deleteDialog.Show (transaction, "deleteDialog");
+				var transaction = FragmentManager.BeginTransaction();
+				RoomListDialogFragment roomDialog = new OptionDialogFragment();
+				roomDialog.Show(transaction,"roomListDialog");
 
-				deleteDialog.OnItemSelected += (DialogClickEventArgs a) =>
+				roomDialog.OnItemSelected += (DialogClickEventArgs a) =>
 				{
-					Property.RoomList.Remove (ListAdapter [e.Position]);
-					ListAdapter.NotifyDataSetChanged ();
+					switch (a.Which)
+					{
+						case 0:
+					{
+						Room newRoom = new Room();
+						newRoom.Label = "Kitchen";
+						Property.RoomList.Add (newRoom);
+						ListAdapter.NotifyDataSetChanged ();
+						break;
+					}
+						case 1:
+					{
+						Room newRoom = new Room();
+						newRoom.Label = "Basement";
+						Property.RoomList.Add (newRoom);
+						ListAdapter.NotifyDataSetChanged ();
+						break;
+					}
+						case 2:
+					{
+						Room newRoom = new Room ();
+						newRoom.Label = "Garage";
+						Property.RoomList.Add (newRoom);
+						ListAdapter.NotifyDataSetChanged();
+						break;
+					}
+						case 3:
+					{
+						Room newRoom = new Room ();
+						newRoom.Label = "Kitchen";
+						Property.RoomList.Add (newRoom);
+						ListAdapter.NotifyDataSetChanged ();
+						break;
+					}
+						case 4:
+					{
+						Room newRoom = new Room ();
+						newRoom.Label = "Office";
+						Property.RoomList.Add (newRoom);
+						ListAdapter.NotifyDataSetChanged ();
+						break;
+					}
+						case 5:
+					{
+						Room newRoom = new Room ();
+						newRoom.Label = "Office";
+						Property.RoomList.Add (newRoom);
+						ListAdapter.NotifyDataSetChanged ();
+						break;
+					}
+						case 6:
+					{
+						// Add Code to go to the Edit Room View.
+					}
+					}
 				};
+				//				var PropertyDetails = new Intent (this,typeof(PropertyDetailActivity));
+				//				PropertyStore store = PropertyCollection.SharedCollection.FindPropertyStoreWithID (ListAdapter[e.Position].ID);
+				//				PropertyStore.CurrentStore = store;
+				//				StartActivity (PropertyDetails);
 			};
 			
 
 
 
+
+		}
+
+		private void AddBathroom()
+		{
 
 		}
 
