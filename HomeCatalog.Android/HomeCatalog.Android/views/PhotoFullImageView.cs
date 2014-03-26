7fbf9e35ca7cdbc093cf2213a793ece48909bcac
Zach Starkebaum <zach@relevantwalk.com>
2014-03-25 21:14:19 -0600

    Add photo class

---
+++ HomeCatalog.Android/HomeCatalog.Android/views/PhotoFullImageView.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace HomeCatalog.Android
+{
+	public class PhotoFullImageView : View
+	{
+		public PhotoFullImageView (Context context) :
+			base (context)
+		{
+			Initialize ();
+		}
+
+		public PhotoFullImageView (Context context, IAttributeSet attrs) :
+			base (context, attrs)
+		{
+			Initialize ();
+		}
+
+		public PhotoFullImageView (Context context, IAttributeSet attrs, int defStyle) :
+			base (context, attrs, defStyle)
+		{
+			Initialize ();
+		}
+
+		void Initialize ()
+		{
+		}
+	}
+}
+
