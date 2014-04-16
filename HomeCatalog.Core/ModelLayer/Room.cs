a3014366412bd85f2e0603c3b7cf00657bf7c8ba
Andrew Clawson <andrew.clawson@ucdenver.edu>
2014-04-15 22:06:00 -0600

    Add validation methods on Room class

--- HomeCatalog.Core/ModelLayer/Room.cs
+++ HomeCatalog.Core/ModelLayer/Room.cs
@@ -1,8 +1,9 @@
 using System;
+using System.Collections.Generic;
 using SQLite;
 namespace HomeCatalog.Core
 {
-	public class Room : ISQLListItem
+	public class Room : ISQLListItem, IValidatable
 	{
 		public Room ()
 		{
@@ -11,8 +12,22 @@ namespace HomeCatalog.Core
 		[PrimaryKey, AutoIncrement]
 		public int ID { get; set; }
 		public string Label { get; set; }
-	}
 
+		public List<string> GetValidationErrors (){
+			List<string> ValidationErrors = new List<string> ();
+			if (Label == null) {
+				ValidationErrors.Add ("Room must have a Label assigned to it.");
+			}
+			foreach (Room room in PropertyStore.CurrentStore) {
+				if (Label == room.Label && ID != room.ID) {
+					ValidationErrors.Add("Room is not unique");
+				}
+			}
+			return null;
+
+		}
+	}
+		
 
 }
 
